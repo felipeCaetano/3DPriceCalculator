1f14e1e99a802174ce9a43a418cf5a944e4413ff
felipeCaetano <felipecmelo@gmail.com>
2026-04-14 01:29:28 -0300

    widgetContent bug fix

--- ui/content.py
+++ ui/content.py
@@ -510,7 +510,6 @@ class MainContent(QWidget):
 class WidgetContent(QWidget):
     def __init__(self, mdi):
         super(WidgetContent, self).__init__()
-        self.animation = QPropertyAnimation(self.mdi.menu, b"size")
         self.mdi = mdi
         self.layout = QVBoxLayout()
         self.layout.setContentsMargins(0, 0, 0, 0)
@@ -535,6 +534,7 @@ class WidgetContent(QWidget):
         self.menu_btn.raise_()
 
     def show_menu(self):
+        self.animation = QPropertyAnimation(self.mdi.menu, b"size")
         self.animation.setDuration(150)
         if self.menu_btn.isChecked():
             self.animation.setStartValue(QSize(0, self.mdi.height()))
