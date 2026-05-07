92b0901289d6638c04289da54a68b2657454979a
felipeCaetano <felipecmelo@gmail.com>
2026-05-07 05:55:07 -0300

    configurações init

--- ui/settings.py
+++ ui/settings.py
@@ -1,4 +1,8 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QLabel, QVBoxLayout, QWidget
+
+from ui.stylehelper import form_label, make_divider, styled_combo, \
+    styled_input, STYLE_SHEET
+
 
 class SettingsWidget(QWidget):
     """Página que mostra todas as configurações disponíveis como impressora
@@ -6,3 +10,13 @@ class SettingsWidget(QWidget):
 
     def __init__(self):
         super().__init__()
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        title = QLabel("Configurações")
+        title.setObjectName("MenuTitle")
+
+        layout.addWidget(title)
+        layout.addWidget(make_divider())
+
+        layout.addStretch()
